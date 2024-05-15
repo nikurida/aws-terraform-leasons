86afd4cc64ac7af87f2e3dd5521c80e55f581f81
Ricardo <rikurida@gmail.com>
2024-05-15 00:30:57 -0400

    brief of terraform.tfstate and fix content type s3 objects

--- 01-terraform-basics/04-interpolation-locals-outputs/s3.tf
+++ 01-terraform-basics/04-interpolation-locals-outputs/s3.tf
@@ -6,8 +6,17 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_object" "this" {
-  bucket = aws_s3_bucket.this.bucket
-  key    = "config/${local.ip_file_path}"
-  source = local.ip_file_path
-  etag   = filemd5(local.ip_file_path)
+  bucket       = aws_s3_bucket.this.bucket
+  key          = "config/${local.ip_file_path}"
+  source       = local.ip_file_path
+  etag         = filemd5(local.ip_file_path)
+  content_type = "application/json"
+}
+
+resource "aws_s3_object" "random" {
+  bucket       = aws_s3_bucket.this.bucket
+  key          = "config/${random_pet.bucket.id}.json"
+  source       = local.ip_file_path
+  etag         = filemd5(local.ip_file_path)
+  content_type = "application/json"
 }
